bb5423e048291ac4cf47af323893f62bcaf28114
Bruno Campos <bruno@peopleinteractive.com.br>
2018-01-10 20:34:21 -0200

    Configurando o gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,30 @@
-var gulp = require('gulp');
+var gulp        = require('gulp')
+var pug         = require('gulp-pug')
+var stylus      = require('gulp-stylus')
+var browserSync = require('browser-sync').create() 
+
+gulp.task('stylus', function() {
+    gulp.src('./src/assets/styles/*.styl')
+        .pipe(stylus({
+            compress: true
+        }))
+        .pipe(gulp.dest('./out/assets/styles/'))
+});
+
+gulp.task('pug', function() {
+    gulp.src('./src/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('./out'))
+});
+
+gulp.task('assets-watch', ['pug', 'stylus'], function(done) {
+    browserSync.reload()
+    done()
+})
+
+gulp.task('watch', function() {
+    gulp.watch(['./src/*.pug'], ['pug'])
+    gulp.watch(['./src/assets/styles/*.styl'], ['stylus'])
+})
+
+gulp.task('build', ['pug', 'stylus'])
